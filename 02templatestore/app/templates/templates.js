ec9cb77880e3b87b1892d9ff298fd5afb7ce5341
Mario Rico <mariorico@gmail.com>
2015-10-10 17:35:41 +0100

    Finished project 2

--- 02templatestore/app/templates/templates.js
+++ 02templatestore/app/templates/templates.js
@@ -7,14 +7,26 @@ angular.module('templateStore.templates',['ngRoute'])
         controller: 'TemplatesCtrl'
     }).
     when('/templates/:templateId', {
-        templateUrl: 'templates/templates-details.html',
-        controller: 'Templates-detailsCtrl'
+        templateUrl: 'templates/template-details.html',
+        controller: 'TemplateDetailsCtrl'
     });
 }])
 
-.controller('TemplatesCtrl', ['$scope', function($scope){
-
+.controller('TemplatesCtrl', ['$scope','$http', function($scope, $http){
+  $http.get('json/templates.json').success(function(data){
+    $scope.templates = data;
+  });
 }])
-.controller('templates-detailsCtrl', ['$scope', function($scope){
+.controller('TemplateDetailsCtrl', ['$scope', '$routeParams', '$http', '$filter', function($scope, $routeParams, $http, $filter){
+  var templateId = $routeParams.templateId;
+  $http.get('json/templates.json').success(function(data){
+    $scope.template = $filter('filter')(data, function(d){
+      return d.id == templateId;
+    })[0];
+    $scope.mainImage = $scope.template.images[0].name;
+  });
 
+  $scope.setImage = function(image){
+    $scope.mainImage = image.name;
+  }
 }]);
