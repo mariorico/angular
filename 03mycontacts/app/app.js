834df750c4c2c114eaeb24aa201f4c046dfbaa95
Mario Rico <mariorico@gmail.com>
2015-10-10 18:39:32 +0100

    Adding contact html

--- 03mycontacts/app/app.js
+++ 03mycontacts/app/app.js
@@ -1,8 +1,10 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('mycontacts', [
-  'ngRoute'
+angular.module('myContacts', [
+  'ngRoute',
+  'firebase',
+  'myContacts.contacts'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/contacts'});
